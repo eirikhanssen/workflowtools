70789675b8af594faed4b5e2e1816f59baa82d06
Eirik Hanssen <eirikhanssen79@gmail.com>
2018-02-09 12:33:06 +0100

    replace character entities with proper utf-8 characters as character entities are not regocnized in xml

--- jats2epub-htmlgalley-2-xhtml5-stage1.sed.sh
+++ jats2epub-htmlgalley-2-xhtml5-stage1.sed.sh
@@ -3,4 +3,12 @@ sed '/<html>/,$!d' | \
 sed -r 's!<html>!<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">!' | \
 sed -r 's:(<img[^>]+[^/])>:\1 />:g' | \
 sed -r 's:<meta[^>]+>:<meta charset="utf-8" />:' | \
-sed '/<\/head>/ i\	<link rel="stylesheet" href="/styles/galley/htmlgalley.css" />'
+sed '/<\/head>/ i\	<link rel="stylesheet" href="/styles/galley/htmlgalley.css" />' | \
+# replace some character entities with utf-8 characters
+sed 's/&nbsp;/ /' | \
+sed 's/&oslash;/ø/' | \
+sed 's/&Oslash;/Ø/' | \
+sed 's/&aelig;/æ/' | \
+sed 's/&Aelig;/Æ/' | \
+sed 's/&aring;/å/' | \
+sed 's/&Aring;/Å/'
